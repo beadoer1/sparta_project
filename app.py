9f31dc8cddc8a9ef05955d86b5e063b4685b0514
beadoer1 <beadoer1@gmail.com>
2020-06-18 22:31:15 +0900

    GET/POST, SEARCH
    GET/POST 방식을 통해
    Search 값을 가져온다

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urlparse
+import json
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 
 client = MongoClient('localhost', 27017)
-db = client.dbsparta                   
+db = client.dbsparta
 
 # HTML을 주는 부분
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# 네이버 API Json 형식으로 받기
-import os
-import sys
-import urllib.request
-client_id = ""
-client_secret = ""
-encText = urllib.parse.quote("서울 맛집")
-url = "https://openapi.naver.com/v1/search/local?query=" + encText + "&display=30" + "sort=comment" # 30개까지, Comment 많은 순으로 출력
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
-if(rescode==200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
-else:
-    print("Error Code:" + rescode)
+@app.route('/api/food', methods=['POST'])
+def foods_list():
+    # 네이버 API JSON 형식으로 받기
+    # keyword = request.form['title_give'] #이런식으로 검색어 불러오면 되지 않을까?
+    location_receive = request.form['location_give']
+    url = "https://openapi.naver.com/v1/search/local?query=" + location_receive + " 맛집" + "&display=30" + "&sort=comment"# keyword 삽입 검색
+    client_id = ""
+    client_secret = ""
+    result = requests.get(urlparse(url).geturl(), headers={"X-Naver-Client-Id": client_id, "X-Naver-Client-Secret": client_secret}) # 검색결과(requests 함수 사용)
+    data = result.json()
+    bob = data['items']
+    return  jsonify({'result': 'success', 'foods_list': bob, 'msg':'떠나보자!!!!'}) #네이버 API 검색값 받은건 어떻게 html로 줄 수 있을까...
 
-    
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
